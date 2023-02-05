c8a6458da6008b34da2a322b9eb182e4dec9fbb0
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-02-05 18:07:44 +0600

    [ADD] Use add and multiplication to calculate wood requirements

---
+++ wood.js
@@ -0,0 +1,26 @@
+/* 
+1. chair --> 3 cft
+2. table --> 10 cft
+3. bed --> 50 cft
+
+
+*/
+
+function woodCalculator(chairQuantity, tableQuantity, bedQuantity) {
+  const perChairWood = 3;
+  const perTableWood = 10;
+  const perBedWood = 50;
+
+  const chairWood = perChairWood * chairQuantity;
+  const tableWood = perTableWood * tableQuantity;
+  const bedWood = perBedWood * bedQuantity;
+
+   const totalWood = chairWood + tableWood + bedWood;
+   
+   return totalWood;
+}
+
+const totalWood = woodCalculator(6, 1, 1);
+
+
+console.log(totalWood);
