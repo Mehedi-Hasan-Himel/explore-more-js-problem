ff59980fac3655af6b671090926689dad468dfd6
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-02-06 02:09:26 +0600

    [ADD] comparision operator

---
+++ comparison-operator.js
@@ -0,0 +1,23 @@
+// ৪. তুমি কি দুইটা ভেরিয়েবল এরমধ্যে তুলনা করতে পারো। কম্পারিজন করতে পারো। যে দুইটা ভেরিয়েবল এর মধ্যে প্রথমটা সেকেন্ডটা এর চাইতে ছোট, বড়, অসমান, সমান , ছোট বা সমান, বড় বা সমান। এইগুলা চেক করতে পারো। অর্থাৎ <, >, ==, !=, <=, >= চিহ্নগুলা ব্যবহার করতে পারো। তাহলে দুইটা সংখ্যা টাইপের ভেরিয়েবল ডিক্লেয়ার করে তাদেরকে এই ছয়ভাবে তুলনা করে কোড লিখে ফেলো। 
+
+
+let number1 = 10;
+let number2 = 20;
+
+let smaller = number1 < number2;
+console.log(smaller);
+
+let bigger = number2 > number1
+console.log(bigger);
+
+let equal = number1 == number2
+console.log(equal);
+
+let notEqual = number1 != number2;
+console.log(notEqual);
+
+let lessThanEqual = number1 <= number2
+console.log(lessThanEqual);
+
+let gaterThanEqual = number2 >= number1;
+console.log(gaterThanEqual);
