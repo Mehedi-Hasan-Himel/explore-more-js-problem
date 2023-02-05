4ff0c0f0f2082a1f2c6d8ddb731862d8783f0d77
mehedi hasan himel <mehedihasanhimel89@gmail.com>
2023-02-06 02:48:09 +0600

    [ADD] addition function

---
+++ add.js
@@ -0,0 +1,11 @@
+// ১১. তিনটা সংখ্যার গুনফল বের করে ফাইনাল রেজাল্ট আউটপুট হিসেবে রিটার্ন করতে হবে। তুমি কি সেই কোড লিখতে পারবে। যদি পারো তাহলে সেই কোড লিখে ফেলো।
+
+function add(num1, num2, num3) {
+   let multiply = num1 * num2 * num3;
+   return multiply
+}
+
+
+let result = add(6, 5, 4)
+
+console.log(result);
